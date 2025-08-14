273e00316c3539b5e81666bd3fa4264db7fd6d31
JorgeGomez <Jorge4K69@gmail.com>
2025-08-14 11:37:58 -0300

    empezando con la logica de programacion del codigo

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
-let amigos = [];
+let listaDeAmigos = [];
 
+//Funcion que AGREGA amigos a la lista
+function agregarAmigo(){
+    listaDeAmigos = document.getElementById("amigo").value;
+    
+    if (listaDeAmigos == ""){ 
+        alert("Porfavor ingrese un nombre");
+    }
+    return;
+    limpiarCaja();
+    console.log(listaDeAmigos);
+}
+//Funcion para limpiar la caja de texto
+function limpiarCaja() {
+    document.getElementById("amigo").value = "";
+}    
