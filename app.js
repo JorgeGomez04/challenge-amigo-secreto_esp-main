ae290821fac559579831349879b24746b19edd91
JorgeGomez <Jorge4K69@gmail.com>
2025-08-18 13:32:09 -0300

    termine de agregar las ultimas funcionalidades del codigo

--- app.js
+++ app.js
@@ -1,23 +1,50 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+//variable
 let listaDeAmigos = [];
 
 //Funcion que AGREGA amigos a la lista
 function agregarAmigo(){
-    let amigosEscritos = document.getElementById("amigo").value;
-    
-    if (amigosEscritos == ""){ 
-        alert("Porfavor ingrese un nombre");
-//si el valor escrito es correcto se agrega
-    }else{
-        listaDeAmigos.push(amigosEscritos);
-        console.log(listaDeAmigos);
-    }
-    limpiarCaja();
-    return; 
+        let amigosEscritos = document.getElementById("amigo").value;
+
+        //si el valor es nullo
+        if (amigosEscritos == ""){ 
+            alert("Porfavor ingrese un nombre");
+            
+        //si el valor ya esta en la lista
+        }else if (listaDeAmigos.includes(amigosEscritos)){
+            alert ("Este nombre ya esta en la lista");
+            
+        //si el valor no esta en la lista se lo agrega a "listaDeAmigos"
+        } else {
+            listaDeAmigos.push(amigosEscritos);
+        }
+        document.getElementById("amigo").value = "";
+        mostrarLista()
+        return; 
 }
 
-//Funcion para limpiar la caja de texto
-function limpiarCaja() {
-    document.getElementById("amigo").value = "";
-}    
+//Funcion que muestra los nombres en una lista
+function mostrarLista(){
+        let listaNombres = document.getElementById("listaAmigos");
+        listaNombres.innerHTML = "";
+
+        //recorre la lista, Crea un nuevo elemento de lista, le asigna el nombre de la "ListaDeAmigo" como texto
+        // y finalmente se lo agrega al index.HTML
+        listaDeAmigos.forEach( listaDeAmigos => {
+            let li = document.createElement("li");
+            li.textContent = listaDeAmigos;
+            listaNombres.appendChild(li);
+    });
+}
+
+//Funcion para sortear al amigo secreto
+function sortearAmigo(){
+        if (listaDeAmigos.length === 0){
+            alert("La lista esta vacia, no hay nombres para sortear");
+            return;
+        }
+        let numeroRandom = Math.floor(Math.random() * listaDeAmigos.length);
+        let amigoRandom = listaDeAmigos[numeroRandom];
+        document.getElementById("resultado").innerHTML = `El amigo sorteado es.. !! ${amigoRandom} !!`;
+}
